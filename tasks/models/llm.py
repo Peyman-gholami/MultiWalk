5735e06eac1994ab218e102229e97dc22b1a0893
Peyman Gholami <peyman_gh@Peymans-MBP-2.ht.home>
2024-12-14 22:32:45 -0600

    LLM

--- tasks/models/llm.py
+++ tasks/models/llm.py
@@ -3,5 +3,6 @@ from transformers import AutoModelForCausalLM
 def LLM(model_name):
     model = AutoModelForCausalLM.from_pretrained(
                     model_name,
+                    load_in_8bit=True,
                 )
     return model
