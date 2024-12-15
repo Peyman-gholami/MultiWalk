2596386bd3d467c94b77731cd4b16c38f99bfd57
Peyman Gholami <peyman_gh@Peymans-MBP-2.ht.home>
2024-12-14 22:53:05 -0600

    LLM

--- tasks/models/llm.py
+++ tasks/models/llm.py
@@ -3,8 +3,5 @@ from transformers import AutoModelForCausalLM
 def LLM(model_name):
     model = AutoModelForCausalLM.from_pretrained(
                     model_name,
-                    load_in_8bit=True,
                 )
-    for param in model.parameters():
-        param.requires_grad = True
     return model
